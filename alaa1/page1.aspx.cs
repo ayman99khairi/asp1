bfc72d050a1ca303f4e6499e182dafb04a44c4aa
ReemKhairy1 <71370694+reem1990@users.noreply.github.com>
2020-11-11 18:49:31 +0200

    first commit

---
+++ alaa1/page1.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace alaa1
+{
+    public partial class page1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void button1_click(object sender, EventArgs e)
+        {
+            TextBox2.Text = TextBox1.Text;
+            TextBox1.Text=string.Empty;
+        }
+    }
+}
